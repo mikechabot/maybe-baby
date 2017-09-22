22713c8e47edbb1af16d33ddc48934ef994a279d
Mike Chabot <mike.chabot@live.com>
2017-09-21 21:36:57 -0400

    Updating tonic example

--- example.js
+++ example.js
@@ -2,6 +2,15 @@
 
 const Maybe = require('maybe-baby');
 
+const FLAT_PROPS = {
+    FIRST_NAME: 'firstName',
+    LAST_NAME : 'lastName',
+    ACCT_DTLS : 'accountDetails',
+    INSR_CODE : 'insuranceCode',
+    ADDRESS   : 'address',
+    ZIP_CODE  : 'zipCode'
+};
+
 /**
  * Example domain service that implements maybe-baby to
  * safely retrieve values from the object
@@ -11,31 +20,36 @@ const PersonService = svc = {
     getFirsName (person) {
         return Maybe
             .of(person)
-            .prop('firstName');
+            .prop(FLAT_PROPS.FIRST_NAME);
+    },
+    getLastName (person) {
+        return Maybe
+            .of(person)
+            .prop(FLAT_PROPS.LAST_NAME);
     },
     getAccountDetails (person) {
         return Maybe
             .of(person)
-            .prop('accountDetails');
+            .prop(FLAT_PROPS.ACCT_DTLS);
     },
     getInsuranceCode (person) {
         return Maybe
             .of(person)
             .chain(svc.getAccountDetails)
-            .prop('insuranceCode');
+            .prop(FLAT_PROPS.INSR_CODE);
     },
     getAddress (person) {
         return Maybe
             .of(person)
             .chain(svc.getAccountDetails)
-            .prop('address');
+            .prop(FLAT_PROPS.ADDRESS);
     },
     getZipCode (person) {
         return Maybe
             .of(person)
             .chain(svc.getAccountDetails)
             .chain(svc.getAddress)
-            .prop('zipCode');
+            .prop(FLAT_PROPS.ZIP_CODE);
     }
 };
 
@@ -43,18 +57,21 @@ const person = {
     firstName     : 'John',
     lastName      : null,
     accountDetails: {
-        insuranceCode: 'BDX2321'
+        insuranceCode: 'BDX2321',
+        address      : null
     }
 };
 
 const firstName = PersonService.getFirsName(person);
+const lastName = PersonService.getLastName(person);
 const accountDetails = PersonService.getAccountDetails(person);
 const insuranceCode = PersonService.getInsuranceCode(person);
 const address = PersonService.getAddress(person);
 const zipCode = PersonService.getZipCode(person);
 
-console.log(firstName.join()); // 'John'
-console.log(accountDetails.join()); // { insuranceCode: 'BDX2321' }
-console.log(insuranceCode.join()); // BDX2321
-console.log(address.join()); // undefined
-console.log(zipCode.join()); // undefined
+console.log(firstName.join());
+console.log(lastName.join());
+console.log(accountDetails.join());
+console.log(insuranceCode.join());
+console.log(address.join());
+console.log(zipCode.join());
